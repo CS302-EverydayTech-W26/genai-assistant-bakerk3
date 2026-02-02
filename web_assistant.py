adae9d1ccf8b4672f6f48a927acd9c1844ca9a08
bakerk3 <bakerk3@carleton.edu>
2026-02-02 16:42:36 -0600

    almost exemplary

--- web_assistant.py
+++ web_assistant.py
@@ -1,7 +1,23 @@
 from gemini_client import *
+from flask import Flask
+from flask import render_template
 
 def main():
-    pass
+    app = Flask(__name__)
+
+    client = GeminiClient()
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template('index.html', title='Home')
+        # return 'The Web App with Python Flask!'
+    
+    @app.route('/prompt/<prompt>')
+    def prompt(prompt):
+        return client.generate_response(prompt)
+    
+    app.run(host='0.0.0.0', port=81)
 
 if __name__ == "__main__":
   main()
